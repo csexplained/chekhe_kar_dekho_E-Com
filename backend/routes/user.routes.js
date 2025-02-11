88e559ff9f3b82295ed733400e988f7a9ac4c841
bhanu pratap sisodiya <bhanupss137@gmail.com>
2025-02-11 14:57:44 +0530

    backend build fix

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -8,9 +8,7 @@ import {
     getCurrentUser,
     updateAccountDetails,
 } from "../controllers/user.controller.js";
-import { upload } from '../middlewares/multer.middleware.js'
 import { verifyjwt } from "../middlewares/auth.middleware.js";
-import { addedusertoreqdontstopresponse } from "../middlewares/authnotstopreq.middleware..js";
 
 const router = Router();
 router.route('/register').post(registerUser);
