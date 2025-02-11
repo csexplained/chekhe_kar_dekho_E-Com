1e0237a115319c1addcce9d2d58efc5dd74a9ebd
bhanu pratap sisodiya <bhanupss137@gmail.com>
2025-02-11 15:28:05 +0530

    import build fix2

--- backend/routes/Order.Routes.js
+++ backend/routes/Order.Routes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import OrderController from '../controllers/Order.controller.js';
-import { verifyjwt } from "../Middlewares/auth.middleware.js";
+import { verifyjwt } from "../Middlewares/Auth.middleware.js";
 import { verifyAdminjwt } from '../Middlewares/AdminAuth.middleware.js';
 
 const router = express.Router();
